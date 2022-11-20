a49121213c1cf947741d8b9736509402fc211b3b
sapucaialuann <luannsapucaia@gmail.com>
2022-11-20 16:26:12 -0300

    Algorithm behaving correctly

--- solution-planeSeats.js
+++ solution-planeSeats.js
@@ -1,53 +1,66 @@
 //reserved seats example from the  '1A 3C 2B 20G 5A' 
 
+//  B C D E F G H J
+
+
 //only three possible seat combinations: (b,c,d,e), (d,e,f,g), (f,g,h,j)
-//therefore, there's no need to take the A and K seats account, bcs it's not gonna change the result.
+    //therefore, there's no need to take the A and K seats accountPossibleSeatCombinations, because it's not gonna change the result, weather it's reserved or not.
+
+const getIndexOfSeats = (reservedSeatsInput) => {
+    return filterList(reservedSeatsInput.split(' '));
+}
+const filterList = (arrayOfSeats) => {
+    const filteredArray = [];
+    arrayOfSeats.forEach(element => {if(!element.match(/[AK]/)) filteredArray.push(element)})
+    // console.log(filteredArray)
+    return filteredArray;
+}
 
 const solution = (N, S) => {
-    const reservedSeatsArray = S.split(' ');
-        const seatColumns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J','K'];
-    let count = 0;
-    
-    const reservedRowNumber = reservedSeatsArray.map(seat => Number(seat.slice(0, -1)));
-    const reservedRowLetter = reservedSeatsArray.map(seat => seat.slice(-1));
+    let countPossibleSeatCombinations = 0;
+    const possibleSeatLetters = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'J']
+
+    const reservedSeatsList = getIndexOfSeats(S);
+
+    const reservedRowNumber = reservedSeatsList.map(seat => Number(seat.slice(0, -1)));
+    const reservedRowLetter = reservedSeatsList.map(seat => seat.slice(-1));
     
     //go through all rows and check for reserved seats
     for(let i = 1; i <= N; i++) {
         //non-empty rows;
         if(reservedRowNumber.includes(i)) {
-            const reservedSeatsInColumn = reservedRowLetter.filter((element, index) => reservedRowNumber[index] === i);
-            const reservedSeatPositions = [];
+            const row = reservedRowLetter.filter((element, index) => reservedRowNumber[index] === i).sort();
 
-            //get the position of every occupied row
-            for(let index in reservedSeatsInColumn) {
-                reservedSeatPositions.push(seatColumns.indexOf(reservedSeatsInColumn[index]));
-            }
-            
-            //for rows with only one seat reserved
-            if(reservedSeatPositions.length == 1) {
-                if (reservedSeatPositions[0] > 8 || reservedSeatPositions[0] < 1) {
-                    count +=2;
-                } 
-                else {
-                    count++;
+            if (row.length === 1) countPossibleSeatCombinations++;
+            else if(row.length <= 4) {
+                console.log('line with reservations: ', i);
+                let countAvailableSeatsInRow = 0;
+                for (let j = 0; j < possibleSeatLetters.length; j++) {
+                    console.log('J: ', possibleSeatLetters[j])
+                    if(!row.includes(possibleSeatLetters[j])) {
+                            countAvailableSeatsInRow++;
+                            console.log('streak: ', countAvailableSeatsInRow);
+                            if (countAvailableSeatsInRow >= 4) {
+                                countPossibleSeatCombinations++
+                                countAvailableSeatsInRow = 0;
+                            }
+                    } else {
+                        countAvailableSeatsInRow = 0;
+                        console.log('streak zerada: ', countAvailableSeatsInRow);
+                    }
                 }
-            }
-            //for seat with multiple seats reserved
-            else {
-                if (reservedSeatPositions.every(el => el > 8 || el < 1)) {
-                    count+=2;
-                }
-                else if( (reservedSeatPositions.length <= 4) && (reservedSeatPositions.every(el => reservedSeatPositions != 4 || el != 5))) {
-                    count++;
-                }
+                countAvailableSeatsInRow = 0;
             }
         }
         //for empty rows
         else {
-            count += 2;
+            countPossibleSeatCombinations += 2;
         }
     }
-    return count;
+    console.log(countPossibleSeatCombinations);
+    return countPossibleSeatCombinations;
 }
+// A B C D E F G H J
+solution(22,'1A 3C 2B 20G 5A')
 
 module.exports = solution;
