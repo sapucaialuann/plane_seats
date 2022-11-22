962e6254adbfd2df962db8dfe0f7fa712a89136b
Luann Sapucaia <37882760+sapucaialuann@users.noreply.github.com>
2022-11-22 16:47:38 +0000

    Add test for empty cases

--- solution-planeSeats.test.js
+++ solution-planeSeats.test.js
@@ -2,3 +2,6 @@ const solution = require('./solution-planeSeats');
 test('returns the combination', () => {
 	expect(solution(22, '1A 3C 2B 20G 5A')).toBe(41);
 });
+test('returns the combination', () => {
+	expect(solution(22, '')).toBe(44);
+});
